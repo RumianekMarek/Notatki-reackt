e0e501f05e6be9288703c772470de50a9522139b
Marek Rumianek <marek.mail7@gmail.com>
2020-10-21 13:24:07 +0200

    add container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './App.scss';
-import List from '../List/ListContainer';
-import Search from '../Search/SearchContainer';
+import Container from '../Container/Container';
 
 class App extends React.Component {
   static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    children: PropTypes.node,
     lists: PropTypes.array,
   }
 
   render() {
-    const {title, subtitle, lists} = this.props;
+    const {lists} = this.props;
     return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-      </main>
+      <Container lists={lists}/>
     );
   }
 }
