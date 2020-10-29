aaeb37deb7f72a6d526ddd6830b9bf31d96252e6
Marek Rumianek <marek.mail7@gmail.com>
2020-10-29 12:22:57 +0100

    removed errors 1

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './SearchResults.scss';
+import styles from './SearchResults.scss' ;
 import Card from '../Card/Card';
 
 class SearchResults extends React.Component {
