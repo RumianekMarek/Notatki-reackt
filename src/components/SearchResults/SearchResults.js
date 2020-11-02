3bbe913256927085eb869e942e0d7e4af4a8e134
Marek Rumianek <marek.mail7@gmail.com>
2020-11-02 13:02:39 +0100

    full app

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import styles from './SearchResults.scss';
+import styles from './SearchResults.scss';
 import Card from '../Card/Card';
 
 class SearchResults extends React.Component {
@@ -12,7 +12,7 @@ class SearchResults extends React.Component {
   render() {
     const {cards} = this.props;
     return (
-      <section /*className={styles.component}*/>
+      <section className={styles.component}>
         <div>
           {cards.map(cardData => (
             <Card key={cardData.id} {...cardData} />
